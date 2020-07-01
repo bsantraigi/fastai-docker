de271a9019edd9d35703ebdac7ca42c1a53fc886
BISHAL <bsantraigi@gmail.com>
2020-07-01 18:33:32 +0000

    FINAL: DONT EVER ChANGE AGAIN

--- fastai-v4-lab/extensions.sh
+++ fastai-v4-lab/extensions.sh
@@ -3,11 +3,8 @@ conda install -y -c conda-forge jupyter_contrib_nbextensions
 jupyter serverextension enable --py jupyterlab --sys-prefix \
   && jupyter nbextension enable --py widgetsnbextension --sys-prefix \
   && jupyter contrib nbextension install --sys-prefix \
-  && jupyter labextension install @jupyter-widgets/jupyterlab-manager \
-  && jupyter labextension install @oriolmirosa/jupyterlab_materialdarker \
-  && jupyter labextension install @ryantam626/jupyterlab_code_formatter \
+#   && jupyter labextension install @jupyter-widgets/jupyterlab-manager \
+#   && jupyter labextension install @oriolmirosa/jupyterlab_materialdarker \
+#   && jupyter labextension install @ryantam626/jupyterlab_code_formatter \
   && jupyter labextension install @aquirdturtle/collapsible_headings \
   && jupyter labextension install @jupyterlab/toc
-  
-pip install jupyterlab_code_formatter \
-  && jupyter serverextension enable --py jupyterlab_code_formatter --sys-prefix
